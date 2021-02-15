49361c1ac49b1b4c60d8dcfe34b9b5b13a58bce5
Ryaford <aloisius.ryan@student.umn.ac.id>
2021-02-15 12:42:55 +0700

    Penambahan Week #

---
+++ app/src/main/java/com/example/week03_29072/DaftarKataAdapter.java
@@ -0,0 +1,25 @@
+package com.example.week03_29072;
+
+import androidx.recyclerview.widget.RecyclerView;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class DaftarKataAdapter extends RecyclerView.Adapter<DaftarKataAdapter.KataViewHolder>
+{
+    @NonNull
+    @Override
+    public DaftarKataAdapter.KataViewHolder onCreateViewHolder
+            (@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+    @Override
+    public void onBindViewHolder
+            (@NonNull DaftarKataAdapter.KataViewHolder holder, int position) {
+
+    }
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
