f5a5b0db3ec1cb9f8c3e004f506a2dc31d54234c
Ryaford <aloisius.ryan@student.umn.ac.id>
2021-02-21 19:29:38 +0700

    Pengerjaan Week 4

---
+++ app/src/main/java/com/example/week04_29072/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.week04_29072;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etIsian, etUrl;
+    private Button btnKirim, btnBrowse;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etIsian = findViewById(R.id.isian);
+        etUrl = findViewById(R.id.url);
+        btnBrowse = findViewById(R.id.buttonBrowse);
+        btnKirim = findViewById(R.id.buttonKirim);
+
+        btnBrowse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                String urlText = etUrl.getText().toString();
+                if(urlText.isEmpty()){
+                    urlText = "http://www.umn.ac.id/";
+                }
+                Intent browseIntent = new Intent(Intent.ACTION_VIEW);
+                browseIntent.setData(Uri.parse(urlText));
+                if(browseIntent.resolveActivity(getPackageManager()) != null){
+                    startActivity(browseIntent);
+                }
+            }
+        });
+    }
+
+
+}
